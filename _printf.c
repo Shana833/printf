2345a134c1942ab84eec6c272e0fdb406505481a
Shana833 <rokshana833@gmail.com>
2023-06-27 09:24:08 -0700

    correction

--- _printf.c
+++ _printf.c
@@ -38,7 +38,7 @@ int _printf(const char *format, ...)
 			i++;
 			if (format[i] == '\0')
 			{
-				return(-1);
+				return (-1);
 			}
 			if (format[i] == ' ')
 				printed += print_percent(list); /*added to handle _printf("%");*/
